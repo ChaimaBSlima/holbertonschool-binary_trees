7e8f04d9d8d7194e396af243966ec41e190933f7
ChaimaBSlima <chaimabslima@outlook.fr>
2024-01-04 10:17:35 +0100

    task0: new node

---
+++ binary_trees.h
@@ -0,0 +1,39 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/*_____________Basic Binary Tree_____________*/
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+
+/*_____________Binary Search Tree_____________*/
+typedef struct binary_tree_s bst_t;
+
+/*_____________AVL Tree_____________*/
+typedef struct binary_tree_s avl_t;
+
+/*_____________Max Binary Heap_____________*/
+typedef struct binary_tree_s heap_t;
+
+/*_____________function prototypes_____________*/
+
+
+#endif /* BINARY_TREES_H */
