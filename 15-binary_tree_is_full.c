12be71abdc991473ce0d7152faebaa9aada7de86
ChaimaBSlima <chaimabslima@outlook.fr>
2024-01-07 20:22:20 +0100

    change loop to recursion

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,36 +1,33 @@
 #include "binary_trees.h"
 
 /**
+ * recursion - Checks if a binary tree is full recursively.
+ * @tree: A pointer to the root node.
+ *
+ * Return: 1 or 0.
+ */
+int recursion(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
+		    (tree->left == NULL && tree->right != NULL) ||
+		     recursion(tree->left) == 0 ||
+		     recursion(tree->right) == 0)
+			return (0);
+	}
+	return (1);
+}
+
+/**
  * binary_tree_is_full - Checks if a binary tree is full.
  * @tree: A pointer to the root node.
  *
- * Return: 1 or 0
- *
+ * Return: 1 or 0.
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-
-	const binary_tree_t *current;
-	const binary_tree_t *stack[1000];
-	int top = -1;
-
-	stack[++top] = tree;
-
-	while (top >= 0)
-	{
-		current = stack[top--];
-		if ((current->left != NULL && current->right == NULL) ||
-			(current->left == NULL && current->right != NULL))
-		{
-			return (0);
-		}
-
-		if (current->left != NULL)
-			stack[++top] = current->left;
-		if (current->right != NULL)
-			stack[++top] = current->right;
-	}
-	return (1);
+	return (recursion(tree));
 }
